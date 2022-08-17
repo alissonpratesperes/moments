dcd07b7752dfeaaa8f4c05320f8f55b28e4f05f5
alissonpratesperes <a.pratesperes@gmail.com>
2022-08-17 20:14:58 -0300

    💄 Novo Component - NEWMOMENT no Front-End da Aplicação.
    Realizada a Estilização CSS, proprietária mas sem fugir muito do Escopo do Workshop, da Página - NewMoment da Aplicação, que será responsável pelo Formulário de Cadastro dos Momentos no Banco de Dados - MongoDB;
    Realizados ajustes gerais de acordo com o padrão de desenvolvimento seguido até aqui, no Front-End da Aplicação.

--- frontend/src/pages/NewMoment.js
+++ frontend/src/pages/NewMoment.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
+import './NewMoment.css';
+
     class NewMoment extends Component {
         render() {
             return (
-                <h1> NewMoment </h1>
+                <form id="new_moment">
+                    <input type="file"/>
+                    <input type="text" name="author" placeholder="Autor do momento" />
+                    <input type="text" name="place" placeholder="Local do momento" />
+                    <input type="text" name="description" placeholder="Descrição do momento" />
+                    <input type="text" name="hashtags" placeholder="Hashtags do momento" />
+                        <button type="submit">Enviar</button>
+                </form>
             );
         };
     }
