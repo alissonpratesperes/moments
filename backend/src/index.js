06ae0e7e31e03c8c6e6fcda791f04b6322bf1d0e
alissonpratesperes <a.pratesperes@gmail.com>
2022-08-16 23:27:52 -0300

    ➕ Instalação de nova Dependência - CORS no Back-End da Aplicação.
    Instalada nova Dependência - CORS, ou, Cross Origin Resource Sharing, para possibilitar o acesso ao Back-End pelo Front-End inclusive já realizada a configuração de uso do CORS em "./backend/src/index.js", no Back-End da Aplicação.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
@@ -8,6 +9,7 @@ const app = express();
         useNewUrlParser: true
     });
 
+        app.use(cors());
         app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
         app.use(require('./routes'));
         app.listen(3333);
