d1fe826c0e93c2ac583ca0485e18bce7c45819cb
alissonpratesperes <a.pratesperes@gmail.com>
2022-08-17 22:49:10 -0300

    🗃️ Integração da Página - NEWMOMENT do Front-End com o Back-End da Aplicação.
    Realizada a Integração completa da Página - NEWMOMENT com o Formulário de Cadastro de Momentos com o Back-End da Aplicação.

--- frontend/src/pages/NewMoment.js
+++ frontend/src/pages/NewMoment.js
@@ -2,22 +2,78 @@ import React, { Component } from 'react';
 
 import send from '../assets/send.svg';
 import './NewMoment.css';
+import api from '../services/api';
 
     class NewMoment extends Component {
-        render() {
-            return (
-                <form id="new_moment">
-                    <input type="file"/>
-                    <input type="text" name="author" placeholder="Autor do momento" />
-                    <input type="text" name="place" placeholder="Local do momento" />
-                    <input type="text" name="description" placeholder="Descrição do momento" />
-                    <input type="text" name="hashtags" placeholder="Hashtags do momento" />
-                        <button type="submit">
-                            <img src={send}/>
-                        </button>
-                </form>
-            );
+        state = {
+            image: null,
+            author: '',
+            place: '',
+            description: '',
+            hashtags: ''
         };
+
+            handleImageChange = event => {
+                this.setState({
+                    image: event.target.files[0]
+                });
+            };
+            handleChange = event => {
+                this.setState({
+                    [ event.target.name ]: event.target.value
+                });
+            };
+            handleSubmit = async event => {
+                event.preventDefault();
+                    const momentData = new FormData();
+                        momentData.append('image', this.state.image);
+                        momentData.append('author', this.state.author);
+                        momentData.append('place', this.state.place);
+                        momentData.append('description', this.state.description);
+                        momentData.append('hashtags', this.state.hashtags);
+                            await api.post('moments', momentData);
+                                this.props.history.push('/');
+            };
+
+                render() {
+                    return (
+                        <form id="new_moment" onSubmit={this.handleSubmit}>
+                            <input type="file"
+                                onChange={this.handleImageChange}
+                            />
+                            <input
+                                type="text"
+                                name="author"
+                                placeholder="Autor do momento"
+                                    onChange={this.handleChange}
+                                    value={this.state.author}
+                            />
+                            <input
+                                type="text"
+                                name="place"
+                                placeholder="Local do momento"
+                                    onChange={this.handleChange}
+                                    value={this.state.place}
+                            />
+                            <input
+                                type="text"
+                                name="description"
+                                placeholder="Descrição do momento"
+                                    onChange={this.handleChange}
+                                    value={this.state.description}
+                            />
+                            <input
+                                type="text" name="hashtags"
+                                placeholder="Hashtags do momento"
+                                    onChange={this.handleChange}
+                                    value={this.state.hashtags}
+                            />
+                                <button type="submit">
+                                    <img src={send} alt="Cadastrar momento"/>
+                                </button>
+                        </form>
+                    );
+                };
     }
 
         export default NewMoment;
